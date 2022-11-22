7ea1199111b00bc16f84dc6636605ccaa824e30e
Edward Monk <edwardmonk@users.noreply.github.com>
2022-11-22 16:48:40 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php echo '<h1>Hello World</h2>'; ?> 
+  <i>The quick brown fox jumpled over the lazy dog.</i>
   
 <?php phpinfo(); ?>
 

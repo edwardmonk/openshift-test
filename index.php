1c2cb2a8d3483eac1f77e59af411b4aa7db77bba
Edward Monk <edwardmonk@users.noreply.github.com>
2022-11-23 08:47:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,10 @@
  <body>
  <?php echo '<h1>Hello World</h2>'; ?> 
   <b>The quick brown fox jumpled over the lazy dog.</b>
-  
+  <p>
+   Molestie erat mi massa amet nunc congue nec lacus dolor maecenas sollicitudin leo magna sed congue morbi morbi eros facilisis tincidunt suspendisse placerat arcu lacus.
+   Nisl suspendisse mi placerat scelerisque eu cursus vivamus purus et id quam tincidunt massa sit et tincidunt nunc eget purus metus proin felis tempus scelerisque.
+  </p>
 <?php phpinfo(); ?>
 
  </body>
